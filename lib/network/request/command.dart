8e06810ee09f6218b26fec9709dc56455c21b369
酸性量子 <lucidquantum@163.com>
2023-05-16 16:02:29 +0800

    更新需要登录的command

--- lib/network/request/command.dart
+++ lib/network/request/command.dart
@@ -1,13 +1,16 @@
 import 'package:meta/meta.dart';
 import 'package:models/network/request/request.dart';
+import 'package:tools/refuse.dart';
+
+import '../../user/user.dart';
 
 /// 服务端使用，定义了处理某种Request的方式
 ///
 /// 如果执行没有问题，并且不需要返回任何数据，那么只需要服务器滴一下就可以了
 /// 如果遇到问题，那么直接抛出[Refuse]（这会由服务器默认捕捉，然后用它自己的方式返回）
-abstract class Executor {
+abstract class Command {
   final Request request;
-  Executor(this.request);
+  Command(this.request);
 
   /// 1. 检查输入是否符合格式
   void inputCheck();
@@ -25,5 +28,13 @@ abstract class Executor {
     await prerequisiteCheck();
     final result = await execute();
     return result;
+  }
+}
+
+abstract class LoginRequiredCommand extends Command {
+  late final User user;
+  LoginRequiredCommand(super.request, {required User? user}) {
+    if (user == null) throw Refuse("无法找到用户，请先登录");
+    this.user = user;
   }
 }
