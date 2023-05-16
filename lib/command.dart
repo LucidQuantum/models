3a9b4ccce2512bd21f7495733538d18fdac650a2
酸性量子 <lucidquantum@163.com>
2023-05-16 11:45:37 +0800

    有些指令要求登录

--- lib/command.dart
+++ lib/command.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:models/json.dart';
 import 'package:models/serializable.dart';
+import 'package:models/user/user.dart';
 
 abstract class Command implements Serializable {
   final String name;
@@ -7,5 +10,16 @@ abstract class Command implements Serializable {
 
   Command(this.name, {this.data});
 
+  /// 如果不报错就是成功，也可能会返回一些数据
   Future execute();
+}
+
+/// 要求登录才能执行的指令
+abstract class LoginRequiredCommand extends Command {
+  late final User user;
+  LoginRequiredCommand(String name, {required User? user, required super.data})
+      : super(name) {
+    if (user == null) throw WebSocketException("无法找到用户，请先登录");
+    this.user = user;
+  }
 }
