704d1094281825c429cf3774197ee420a5d3e6a2
酸性量子 <lucidquantum@163.com>
2023-05-16 10:49:16 +0800

    更新版本

--- lib/network/request/request.dart
+++ lib/network/request/request.dart
@@ -12,30 +12,26 @@ part 'request.g.dart';
 
 @JsonSerializable()
 class Request implements Serializable {
-  /// 如果有id，就是某个特定的回复，如果没有id，那就是通用的回复
-  final String? id;
+  final String id;
   final String command;
   final Map<String, dynamic>? data;
 
-  const Request({this.id, required this.command, required this.data});
+  const Request._(
+      {required this.id, required this.command, required this.data});
+
   factory Request.create(
           {required String command, Map<String, dynamic>? data}) =>
-      Request(id: Generator.id(), command: command, data: data);
+      Request._(id: Generator.id(), command: command, data: data);
 
   factory Request.fromJson(Map<String, dynamic> json) =>
       _$RequestFromJson(json);
   Map<String, dynamic> toJson() => _$RequestToJson(this);
 
-  /// 从Request中稳定获取一个数值，否则报错
-  T extract<T>(String key) {
-    if (data == null) throw Exception("data为空，请补充");
-    if (!data!.containsKey(key)) throw Exception("数据中缺少$key");
-    if (data![key] is! T) throw Exception("数据$key的格式不正确，应该为${T.toString()}");
-
-    return data![key];
-  }
-
+  /// 等待回复
+  /// 根据id识别，一旦收到了准确的回复，就会关闭监听
   Future<Response> waitForReply(Stream broadcast) async {
+    assert(broadcast is! WebSocket);
+
     final completer = Completer<String>();
     late StreamSubscription subscription;
 
@@ -52,6 +48,17 @@ class Request implements Serializable {
     final response = Response.fromJson(jsonDecode(json));
 
     return response;
+  }
+
+  /// 服务端
+
+  /// 从Request的dada中稳定获取一个数值，否则报错
+  T extract<T>(String key) {
+    if (data == null) throw Exception("data为空，请补充");
+    if (!data!.containsKey(key)) throw Exception("数据中缺少$key");
+    if (data![key] is! T) throw Exception("数据$key的格式不正确，应该为${T.toString()}");
+
+    return data![key];
   }
 
   reply(WebSocket socket, Response response) {
