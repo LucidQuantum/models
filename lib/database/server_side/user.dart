e5d721a6b151fa91c1c3549eb4421ac83ca55122
酸性量子 <lucidquantum@163.com>
2023-05-19 11:18:07 +0800

    更新User的高级方法

--- lib/database/server_side/user.dart
+++ lib/database/server_side/user.dart
@@ -7,4 +7,14 @@ class Users extends MongoDAO<User> {
 
   @override
   User Function(Json json) get unserialize => User.fromJson;
+
+  Future<User> findOrCreate(String phone) async {
+    User? user = await findOne({"phone": phone});
+    if (user == null) {
+      user = User.create(phone);
+      await insert(user);
+    }
+
+    return user;
+  }
 }
