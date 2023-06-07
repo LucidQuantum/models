96b19a14aaf969e6e01bedf87e816159dcfc59dd
酸性量子 <lucidquantum@163.com>
2023-06-08 02:46:59 +0800

    添加request的公用名字

---
+++ lib/network/client_side/request/name.dart
@@ -0,0 +1,9 @@
+class REQUEST {
+  static const String addTarget = "add_target";
+  static const String startTarget = "start_target";
+  static const String finishTarget = "finish_target";
+  static const String changeTarget = "change_target";
+  static const String switchDone = "switch_done";
+  static const String switchDropped = "switch_dropped";
+  static const String fetch = "fetch";
+}
